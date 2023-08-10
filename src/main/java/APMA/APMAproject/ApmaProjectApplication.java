b34f5893d629c94a97dc0a1ccf7b1291df853161
gujaeseong <gjs5448@gmail.com>
2023-08-10 15:53:26 +0900

    Swagger /docs 로 접근 가능하도록 변경

--- src/main/java/APMA/APMAproject/ApmaProjectApplication.java
+++ src/main/java/APMA/APMAproject/ApmaProjectApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
+
+import static org.springframework.web.servlet.function.RequestPredicates.GET;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 @SpringBootApplication
 public class ApmaProjectApplication {
@@ -19,4 +26,9 @@ public class ApmaProjectApplication {
 		SpringApplication.run(ApmaProjectApplication.class, args);
 	}
 
+	@Bean
+	RouterFunction<ServerResponse> routerFunction() {
+		return route(GET("/docs"), req ->
+				ServerResponse.temporaryRedirect(URI.create("swagger-ui/index.html")).build());
+	}
 }
