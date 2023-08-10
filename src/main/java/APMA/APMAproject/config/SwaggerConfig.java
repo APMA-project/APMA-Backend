9d169c2c00227a740e43594162ac77d6b2ffbea8
gujaeseong <gjs5448@gmail.com>
2023-08-10 15:59:37 +0900

    Swagger 수정

--- src/main/java/APMA/APMAproject/config/SwaggerConfig.java
+++ src/main/java/APMA/APMAproject/config/SwaggerConfig.java
@@ -2,29 +2,79 @@ package APMA.APMAproject.config;
 
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-        info = @Info(title = "APMA",
-                description = "APMA api명세",
-                version = "v1"))
-@RequiredArgsConstructor
+
+/**
+ * Swagger springdoc-ui 구성 파일
+ */
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public GroupedOpenApi chatOpenApi() {
-        String[] paths = {"/APMA/**"};
-
+    public GroupedOpenApi group1() {
         return GroupedOpenApi.builder()
-                .group("APMA API v1")
-                .pathsToMatch(paths)
+                .group("ALL")
+                .pathsToMatch("/**")
+                // .packagesToScan("com.example.swagger") // package 필터 설정
                 .build();
     }
 
+    @Bean
+    public GroupedOpenApi group2() {
+        return GroupedOpenApi.builder()
+                .group("ADMIN")
+                .pathsToMatch("/APMA/admin/**","/APMA/member/**")
+                // .packagesToScan("com.example.swagger") // package 필터 설정
+                .build();
+    }
 
+    @Bean
+    public GroupedOpenApi group3() {
+        return GroupedOpenApi.builder()
+                .group("MEMBER")
+                .pathsToMatch("/APMA/member/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi group4() {
+        return GroupedOpenApi.builder()
+                .group("JOIN")
+                .pathsToMatch("/APMA/join/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        io.swagger.v3.oas.models.info.Info info = new Info()
+                .version("v1.0.0")
+                .title("PROJECT APMA API Document");
+//                .description("API Description");
+
+        // SecuritySecheme명
+        String jwtSchemeName = "JWT Token";
+        // API 요청헤더에 인증정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .scheme("bearer"));
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
 }
