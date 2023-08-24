7710f7e841d5538ad052a447ca0c8343ebd374e0
githyuniiee <https://github.com/githyuniiee>
2023-08-24 09:00:33 +0900

    cors 에러 문제

--- src/main/java/APMA/APMAproject/ApmaProjectApplication.java
+++ src/main/java/APMA/APMAproject/ApmaProjectApplication.java
@@ -1,5 +1,7 @@
 package APMA.APMAproject;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +14,7 @@ import java.net.URI;
 import static org.springframework.web.servlet.function.RequestPredicates.GET;
 import static org.springframework.web.servlet.function.RouterFunctions.route;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://apma2023.net", description = "Default Server URL")} )
 @SpringBootApplication
 public class ApmaProjectApplication {
 
